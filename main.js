f7c44a43dab5ea58af12f382aeb257d309b1cef2
MM <phacharanan18@gmail.com>
2023-06-29 00:21:52 +0700

    2.4

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const { createApp, ref } = Vue
 
 createApp({
     setup(){
-        const product = ref('Socks')
+        // const product = ref('Socks')
+        const product = ref('Boots')
         return {
             product
         }
