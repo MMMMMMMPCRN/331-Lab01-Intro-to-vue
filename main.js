8e361d4ec24fb6fcac190aaf4d36a64e5441e46a
MM <phacharanan18@gmail.com>
2023-06-29 20:38:37 +0700

    9.1-9.5 ProductDisplay.js

--- main.js
+++ main.js
@@ -1,100 +1,18 @@
 const { createApp, ref, computed } = Vue 
 
-createApp({
+const app = createApp({
+
     setup(){
-        // const product = ref('Socks')
-        const product = ref('Boots')
-
-        const brand = ref('SE 331')
-
-        const desc = ref('Boots description')
-
-        // const image = ref('./assets/images/socks_green.jpg')
-
-        const link = ref('https://www.camt.cmu.ac.th')
-
-        // const inStock = ref(true)
-
-        // const inStock = ref(false)
-
-        const inventory = ref(100)
-
-        const onSale = ref(true)
-
-        const details = ref([
-            '50% cotton',
-            '30% wool',
-            '20% polyester'
-        ])
-
-        const variants = ref([
-            // { id: 2234, color: 'green' },
-            // { id: 2235, color: 'blue' }
-            { id: 2234, color: 'green', image:'./assets/images/socks_green.jpg', quantity: 50 },
-            { id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg', quantity: 0 }
-        ])
-
-        const selectedVariant = ref(0)
-
-        function updateVariant(index){
-            selectedVariant.value = index;
-        }
-
-        const image = computed(() => {
-            return variants.value[selectedVariant.value].image
-        })        
-
-        const inStock = computed((() => {
-            return variants.value[selectedVariant.value].quantity
-        }))
-
-        const sizes = ref([
-            'S', 'M', 'L'
-        ])
-
         const cart = ref(0)
-            function addToCart() {
-                cart.value += 1
-            }
-
-            const title = computed(() =>{
-                // return brand.value + ' ' + product.value
-                const baseTitle =  brand.value + ' ' + product.value
-                if (onSale.value) {
-                    return baseTitle + ' is on sale'
-                } else {
-                    return baseTitle
-                }
-
-            })
-
-            function updateImage(variantImage){
-                image.value = variantImage
-            }
-
-            function toggleStockStatus() {
-                inStock.value = !inStock.value;
-            }
-
         return {
-            // product,
-            desc,
-            image,
-            link,
-            inStock,
-            inventory,
-            onSale,
-            details,
-            variants,
-            sizes,
-            cart,
-            addToCart,
-            updateImage,
-            toggleStockStatus,
-            // brand,
-            title,
-            updateVariant
+            cart
         }
     }
-}).mount('#app')
+
+// }).mount('#app')
+})
+
+app.component('product-display', productDisplay)
+
+app.mount('#app')
 
