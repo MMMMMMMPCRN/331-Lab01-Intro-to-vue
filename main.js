c615b7db2dd222f19069b2907fd7d186ca42fe3c
MM <phacharanan18@gmail.com>
2023-06-29 15:12:04 +0700

    8.3-8.4

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ createApp({
 
         const desc = ref('Boots description')
 
-        const image = ref('./assets/images/socks_green.jpg')
+        // const image = ref('./assets/images/socks_green.jpg')
 
         const link = ref('https://www.camt.cmu.ac.th')
 
         // const inStock = ref(true)
 
-        const inStock = ref(false)
+        // const inStock = ref(false)
 
         const inventory = ref(100)
 
@@ -30,9 +30,23 @@ createApp({
         const variants = ref([
             // { id: 2234, color: 'green' },
             // { id: 2235, color: 'blue' }
-            { id: 2234, color: 'green', image:'./assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg' }
+            { id: 2234, color: 'green', image:'./assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue', image:'./assets/images/socks_blue.jpg', quantity: 0 }
         ])
+
+        const selectedVariant = ref(0)
+
+        function updateVariant(index){
+            selectedVariant.value = index;
+        }
+
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })        
+
+        const inStock = computed((() => {
+            return variants.value[selectedVariant.value].quantity
+        }))
 
         const sizes = ref([
             'S', 'M', 'L'
